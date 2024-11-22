06a81a82cfbe876cde40d283a68d995085bff367
thierrysio <thierry28220@icloud.com>
2024-11-22 10:47:52 +0100

    ok

--- assets/app.js
+++ assets/app.js
@@ -1,4 +1,4 @@
-import './bootstrap.js';
+
 /*
  * Welcome to your app's main JavaScript file!
  *
@@ -8,3 +8,8 @@ import './bootstrap.js';
 import './styles/app.css';
 
 console.log('This log comes from assets/app.js - welcome to AssetMapper! 🎉');
+
+import { createApp } from 'vue';
+import Hello from './composants/hello.vue';
+
+createApp(Hello).mount('#app');
---
+++ assets/composants/hello.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+      <h1>{{ message }}</h1>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    data() {
+      return {
+        message: 'Hello from Vue 3!'
+      };
+    }
+  };
+  </script>
+  
+  <style scoped>
+  h1 {
+    color: #42b983;
+  }
+  </style>
+  
